c4a030f8e721e933af5df4b4a6a861a1a7d18d0a
Kittenlover229 <artur.roos@mail.ru>
2021-03-30 01:34:17 +0300

    feat: Draw the triangle

--- main.cpp
+++ main.cpp
@@ -38,7 +38,16 @@ bool Asteroids::OnUserUpdate(float deltaTime) {
 }
 
 void Procedures::DrawShip() {
-    // nothing yet
+    olc::vf2d a, b, c;
+    olc::vf2d center = { (float)asteroids->ScreenWidth() / 2, (float)asteroids->ScreenHeight() / 2};
+
+    a = { center.x, center.y - (float)5 };
+    b = { center.x - (float)4, center.y + (float)2.5 };
+    c = { center.x + (float)4, center.y + (float)2.5 };
+
+    asteroids->DrawLine(a, b);
+    asteroids->DrawLine(b, c);
+    asteroids->DrawLine(c, a);
 }
 
 
