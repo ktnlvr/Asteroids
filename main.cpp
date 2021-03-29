3f30c1f8f08eee9b6ba65fd48da2215e467c3f85
Kittenlover229 <artur.roos@mail.ru>
2021-03-30 01:07:39 +0300

    feat: Basic empty screen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+
+#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+#define OK 1
+
+struct Asteroids : public olc::PixelGameEngine {
+    bool OnUserCreate() override;
+    bool OnUserUpdate(float) override;
+};
+
+
+bool Asteroids::OnUserCreate() {
+    return OK;
+}
+
+bool Asteroids::OnUserUpdate(float deltaTime) {
+    return OK;
+}
+
+
 int main(int argc, char* argv[]) {
-    std::cout << "Asteroids..." << std::endl;
-    return !std::cout;
+    Asteroids asteroids = Asteroids();
+
+    if (asteroids.Construct(128, 128, 4, 4))
+        asteroids.Start();
+
+    return 0;
 }
